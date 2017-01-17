49f6d2e67ab999b13aa70edfbd57cd0959f9ca85
Francesco Laurita <taganaka@gmail.com>
2017-01-17 11:22:15 -0800

    Better sigchld_handler

--- parallel.c
+++ parallel.c
@@ -7,14 +7,18 @@
 #include <unistd.h>
 #include <ctype.h>
 
-#define VERSION "0.1"
+#define VERSION "0.2"
 #define MAX_CMD_LEN 1024
 #define INITIAL_QUEUE_CAPACITY 100
 
 typedef struct {
   char *command;
   pid_t pid;
+  int status;
 } command_t;
+
+command_t **command_queue;
+size_t number_of_commands;
 
 void usage(const char *name){
   fprintf(stderr, "Parallel version %s. That's it, no bullshit\n", VERSION);
@@ -35,6 +39,22 @@ size_t trim(char *string){
   return strlen(string);
 }
 
+void set_chld_status(const int pid, const int status){
+  for (size_t i = 0; i < number_of_commands; i++){
+    if (command_queue[i]->pid == pid){
+      command_queue[i]->status = status;
+      break;
+    }
+  }
+}
+
+void sigchld_handler(int signum){
+    pid_t pid;
+    int   status;
+    pid = waitpid(-1, &status, WNOHANG);
+    set_chld_status(pid, status);
+}
+
 int main(const int argc, const char **argv){
 
   if (argc < 2){
@@ -48,13 +68,15 @@ int main(const int argc, const char **argv){
     return EXIT_FAILURE;
   }
 
+  signal(SIGCHLD, sigchld_handler);
+
   size_t number_of_cpus = sysconf(_SC_NPROCESSORS_ONLN);
-  size_t number_of_commands = 0;
+  number_of_commands = 0;
   size_t q_capacity = INITIAL_QUEUE_CAPACITY;
-  command_t **q = malloc(q_capacity * sizeof **q);
+  command_queue = malloc(q_capacity * sizeof **command_queue);
   size_t *running_proc = mmap(NULL, sizeof *running_proc, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
   *running_proc = 0;
-  if (q == NULL){
+  if (command_queue == NULL){
     perror("malloc");
     return EXIT_FAILURE;
   }
@@ -64,8 +86,8 @@ int main(const int argc, const char **argv){
     if (trim(command) == 0)
       continue;
 
-    q[number_of_commands] = malloc(sizeof(command_t));
-    command_t *cmd = q[number_of_commands];
+    command_queue[number_of_commands] = malloc(sizeof(command_t));
+    command_t *cmd = command_queue[number_of_commands];
     if (cmd == NULL){
       perror("malloc");
       return EXIT_FAILURE;
@@ -79,7 +101,11 @@ int main(const int argc, const char **argv){
     number_of_commands++;
     if (number_of_commands == q_capacity){
       q_capacity *= 2;
-      q = realloc(q, q_capacity * sizeof **q);
+      command_queue = realloc(command_queue, q_capacity * sizeof **command_queue);
+      if (command_queue == NULL){
+        perror("realloc");
+        return EXIT_FAILURE;
+      }
     }
   }
   fclose(fp);
@@ -92,34 +118,40 @@ int main(const int argc, const char **argv){
     }
 
     if (pid == 0){
-      int stat = system(q[i]->command);
+      int stat = system(command_queue[i]->command);
       (*running_proc)--;
       munmap(running_proc, sizeof *running_proc);
       exit(WEXITSTATUS(stat));
     } else {
       (*running_proc)++;
-      q[i]->pid = pid;
+      command_queue[i]->pid = pid;
     }
 
     while(*running_proc >= number_of_cpus){
       sleep(1);
     }
   }
+
+  while (*running_proc > 0) {
+    sleep(1);
+  }
+
   bool all_good = true;
   for (size_t i = 0; i < number_of_commands; i++){
-    int status;
-    waitpid(q[i]->pid, &status, 0);
-
-    if (!WIFEXITED(status) || WEXITSTATUS(status) != 0){
-      fprintf(stderr, "Command [%s] with PID %d ret status != 0, exit status: %d\n", q[i]->command, q[i]->pid, WEXITSTATUS(status));
+    if (!WIFEXITED(command_queue[i]->status) || WEXITSTATUS(command_queue[i]->status) != 0){
+      fprintf(stderr, "Command [%s] with PID %d ret status != 0, exit status: %d\n",
+        command_queue[i]->command,
+        command_queue[i]->pid,
+        WEXITSTATUS(command_queue[i]->status)
+      );
       all_good = false;
     }
 
-    free(q[i]->command);
-    free(q[i]);
+    free(command_queue[i]->command);
+    free(command_queue[i]);
   }
 
-  free(q);
+  free(command_queue);
   munmap(running_proc, sizeof *running_proc);
   return all_good ? EXIT_SUCCESS : EXIT_FAILURE;
 }
